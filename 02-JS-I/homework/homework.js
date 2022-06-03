b24c331a7a53ba4e7c84c8af2e41e9ea4ba61d85
Wilson Huallpa <wilsonhuallpapro@gmail.com>
2022-06-02 23:41:16 -0300

    finalized JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,14 +226,14 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado * 4 ;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  return ((base * altura)/2);
 }
 
 
@@ -241,7 +241,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  let dolar = 1.20;
+  return euro * dolar;
 }
 
 
@@ -251,11 +252,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    return 'Es vocal';
+   } else if (letra.length !== 1) {
+    return 'Dato incorrecto';
+   }else{
+    return 'Dato incorrecto';
+   }
 }
-
-
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
